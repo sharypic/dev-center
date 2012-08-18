8f8b3a56b4168758ddc48ddc9580c9dc5bb35b33
Christian Blavier <cblavier@gmail.com>
2012-08-18 09:35:20 +0200

    new try with generated site (to preserve list order)

---
+++ _site/javascripts/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  $('.highlight pre').zclip({
+    path: '/dev-center/javascripts/ZeroClipboard.swf',
+    copy: function() { return $(this).text(); },
+    afterCopy: function() { $(this).removeClass('hover') }
+  });
+
+  $('.see-response').click(function(e) {
+    var $parent = $(this).parents('.service');
+    $(".sample-response", $parent).collapse('toggle');
+    $('.see-response i', $parent).toggleClass('icon-chevron-up');
+    $('.see-response i', $parent).toggleClass('icon-chevron-down');
+  });
+
+});
